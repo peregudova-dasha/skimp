7841ed974c555d08d8234899b1c6bf1b7f756a91
Dasha <dasha.peregudova@gmail.com>
2016-05-13 10:54:03 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,20 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'dasha.peregudova@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Письмо'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Email: '.$_POST['email'].'</p> 
+                        <p>Текст письма: '.$_POST['message'].'</p> 
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
