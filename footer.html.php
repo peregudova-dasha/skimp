354ff0a80ec987fd83eda17d7d76ce55b22850bc
Dasha <dasha.peregudova@gmail.com>
2016-05-17 17:03:56 +0300

    Create footer.html.php

---
+++ footer.html.php
@@ -0,0 +1,17 @@
+  
+  
+  <footer>
+    <div class="container">
+      <div class="left col-md-4 col-sm-4 col-xs-12">
+        <p>Lorem ipsum dolor Exercitationem corporis sit amet, consectetur adipisicing elit. Exercitationem corporis.</p>
+      </div>
+      <div class="col-md-4 col-sm-4 col-xs-12">
+        <div class="logo"></div>
+        <h4>ООО "СКИМП"</h4>
+        <p>Киев, ул. Максима Кривоноса, 2а</p>
+      </div>
+      <div class="right col-md-4 col-sm-4 col-xs-12">
+        <p> s-seem@ukr.net <br> +38 067 507 60 38 <br> </p>
+      </div>
+    </div>
+  </footer>
